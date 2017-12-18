5a4f70fdb652196d02d01ee01c76ddeabdaae2a1
Leon Shainski <31663389+LeonShaisnki@users.noreply.github.com>
2017-12-18 10:18:11 -0500

    Uploading index.js and style.css

---
+++ index.js
@@ -0,0 +1,68 @@
+var taxi = document.getElementById('car'),
+    moveLeft = document.getElementById('left'),
+    moveRight = document.getElementById('right'),
+    moveUp = document.getElementById('up'),
+    moveDown = document.getElementById('down'),
+    reqID,
+    direction;
+
+//Other car
+var car = document.getElementById('car2'),
+      stopTime,
+      Velocity2;
+
+function DetVelocity() {
+    Velocity2 = 0.05;
+
+}
+
+
+
+function changeDirection(arrow) {
+  direction = arrow;
+}
+
+function startMoving() {
+  if (direction === 'right') {
+    taxi.style.left = (taxi.offsetLeft += 2) + 'px';
+  } else if (direction === 'left') {
+    taxi.style.left = (taxi.offsetLeft -= 2) + 'px';
+  } else if (direction === 'up') {
+    taxi.style.top = (taxi.offsetTop -= 2) + 'px';
+  } else if (direction === 'down') {
+    taxi.style.top = (taxi.offsetTop += 2) + 'px';
+  }
+
+  // tells the browser to keep running the car 60 frames per second.
+  reqID = window.requestAnimationFrame(startMoving);
+}
+
+function stopMoving() {
+  // cancel requestAnimationFrame function to stop moving.
+  window.cancelAnimationFrame(reqID);
+}
+
+// EventListener Mousedown
+moveUp.addEventListener('mousedown', function() {
+  changeDirection('up');
+  startMoving();
+});
+moveUp.addEventListener('mouseup', stopMoving);
+
+moveDown.addEventListener('mousedown', function() {
+  changeDirection('down');
+  startMoving();
+});
+moveDown.addEventListener('mouseup', stopMoving);
+
+moveLeft.addEventListener('mousedown', function() {
+  changeDirection('left');
+  startMoving();
+});
+moveLeft.addEventListener('mouseup', stopMoving);
+
+moveRight.addEventListener('mousedown', function() {
+  changeDirection('right');
+  startMoving();
+});
+moveRight.addEventListener('mouseup', stopMoving);
